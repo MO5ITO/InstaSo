1a0eb0f7311b236d5609825482e1f7ce442c9bf1
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2023-11-04 00:07:23 +0100

    FrontEnd

--- client/src/Views/Widgets/PageNotFound.jsx
+++ client/src/Views/Widgets/PageNotFound.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import NotFound from "../../Assets/PageNotFound404.png"
+import NavBar from 'Views/NavBar'
+import { Box } from '@mui/material'
+
 
 const PageNotFound = () => {
   return (
-    <div className='pageCenter mt-3'>
-      <img src={NotFound} alt='NotFound404' />
-    </div>
+    <Box>
+      <NavBar/>
+      <div className='pageCenter mt-3'>
+        <img src={NotFound} alt='NotFound404' />
+      </div>
+    </Box>
+
   )
 }
 
