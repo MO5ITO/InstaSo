8835dfae1b5f5a328c76022ad8764b9197c5faf8
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2023-11-05 17:42:29 +0100

    FrontEnd

--- client/src/Views/HomePage/index.jsx
+++ client/src/Views/HomePage/index.jsx
@@ -1,13 +1,23 @@
-import { Box } from '@mui/material'
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
 import NavBar from 'Views/NavBar'
 import React from 'react'
 
 const HomePage = () => {
+
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
   return (
     <Box>
       <NavBar/>
+      <Box width={isNonMobileScreens ? "70%" : "93%"} p="2rem" m="2rem auto" borderRadius="1.5rem" backgroundColor={theme.palette.background.alt} >
+        <Typography fontWeight="500" textAlign="center" variant="h5" sx={{ mb: "1.5rem" }}>
+          NstaSo! Home Page
+        </Typography>
+        </Box>
     </Box>
   )
 }
 
 export default HomePage
+
