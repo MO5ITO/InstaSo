ff2b75330b2355742d76384aecaba27199aa53e1
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2023-12-07 23:58:47 +0100

    NSTASO

--- client/src/Views/Widgets/ShowOnePost.jsx
+++ client/src/Views/Widgets/ShowOnePost.jsx
@@ -59,14 +59,8 @@ import ProfilePicture from "Components/ProfilePicture";
           {description}
         </Typography>
         {picturePath && (
-          <img
-            width="100%"
-            height="auto"
-            alt="post"
-            style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-            src={`http://localhost:3001/assets/${picturePath}`}
-          />
-        )}
+          <img width="100%" height="auto" alt="post" style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }} 
+                src={`http://localhost:3001/assets/${picturePath}`} /> )}
         <FlexBetween mt="0.25rem">
           <FlexBetween gap="1rem">
             <FlexBetween gap="0.3rem">
@@ -116,7 +110,7 @@ import ProfilePicture from "Components/ProfilePicture";
                         borderRadius: "1.5rem",
                         padding: "0.7rem 2rem",
                         }}/>
-                        
+
             <IconButton><AddBoxOutlinedIcon/></IconButton>
         </FlexBetween>
       </Widgets>
