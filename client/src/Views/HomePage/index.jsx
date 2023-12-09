4dcc63a71e00565a68ab310f158bce81e24ad260
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2023-12-09 16:32:39 +0100

    NSTASO

--- client/src/Views/HomePage/index.jsx
+++ client/src/Views/HomePage/index.jsx
@@ -12,8 +12,6 @@ const HomePage = () => {
 
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const { _id, picturePath } = useSelector((state) => state.user);
-  
-  
 
   return (
     <Box>
