46f214e26528e8b10f0386ebfccfb35da87b6c6e
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-01-08 00:12:30 +0100

    Nstaso

--- client/src/Views/Widgets/ShowAllPosts.jsx
+++ client/src/Views/Widgets/ShowAllPosts.jsx
@@ -7,7 +7,6 @@ const ShowAllPosts = ({ userId, isProfile = false }) => {
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts);
     const token = useSelector((state) => state.token);
-  
     const getPosts = async () => {
       const response = await fetch("http://localhost:3001/posts", {
         method: "GET",
