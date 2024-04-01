dabf394a8230e193914578d0a3f78773e018cc43
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2024-04-01 01:36:42 +0100

    Nstaso

--- client/src/index.js
+++ client/src/index.js
@@ -25,7 +25,6 @@ const store = configureStore({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-  
   <BrowserRouter>
       <Provider store={store}>
       <PersistGate loading={null} persistor={persistStore(store)}>
